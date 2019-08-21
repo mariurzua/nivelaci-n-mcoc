99503197143fdf6327e0aac31c7a68f1dc222bbc
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-20 22:48:18 -0400

    Create 000436.py

---
+++ 20082019/000436.py
@@ -0,0 +1,13 @@
+#tiempo video: 2 minutos 27 segundos
+
+def funcion1():                                #definicion de la funcion
+    print ("holaaaaa")                         #impresion dentro de la funcion de un "holaaaaa"
+    print ("holaaaaaaa2")                      #impresion dentro de la funcion (2da) de un "holaaaa2" 
+    
+#tiempo video: 4 minutos 36 segundos 
+#En este ejemplo, se aprende y se puede observar que una funcion puede ser utilizada mas de una vez,
+#por lo que es considerada una coleccion de instrucciones, ayuda a no tener que volver a escribir
+#nuevamente la instruccion
+    
+funcion1()                        #llamado numero 1 a que se ejecute la funcion
+funcion1()                        #llamado numero 2 a que se ejecute la funcion
