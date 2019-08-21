5db2b6ae2ba469e81439adfbc5e6123aa407a20b
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-20 22:47:34 -0400

    Create 000416.py

---
+++ 20082019/000416.py
@@ -0,0 +1,12 @@
+#tiempo video: 2 minutos 27 segundos
+
+def funcion1():                                #definicion de la funcion
+    print ("holaaaaa")                         #impresion dentro de la funcion de un "holaaaaa"
+    print ("holaaaaaaa2")                      #impresion dentro de la funcion (2da) de un "holaaaa2" 
+
+
+#tiempo video: 4 minutos 16 segundos
+
+#En este caso, ya conocida la funcion dada anteriormente, lo que se hace es llamar a la funcion, 
+#que en este caso da 2 impresiones ("holaaaa" y "holaaaa2")
+funcion1()                                     #aqui se llama a la funcion, a que se ejecute
