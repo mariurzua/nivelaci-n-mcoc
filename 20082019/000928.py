955378fcc79b48ce7fa877769f4e7c6da108c008
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-20 22:50:42 -0400

    Create 000928.py

---
+++ 20082019/000928.py
@@ -0,0 +1,10 @@
+# 9 minutos y 28 segundos
+
+#Se aprende a ver como una funcion puede imprimir 2 cosas, llamandola de la manera adecuada.
+#En este caso, se llama con el numero 2, y como existe argumeno, si se imprime esa parte de la funcion.
+
+def funcion5(algun_argumento):      #definicion funcion5 con algun_argumento dentro de ella.
+    print (algun_argumento)         #impresion dentro de la funcion5, que toma ese argumento .
+    print ("wiiii")                 #impresion dentro de la funcion5, que no considera el argumento.
+    
+funcion5(2)                         #llamado a la funcion5 definida, que arroja  impresiones
