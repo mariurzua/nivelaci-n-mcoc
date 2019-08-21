d351104307ef07758b689ae73abe910cb235dc22
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-20 22:49:41 -0400

    Create 000718.py

---
+++ 20082019/000718.py
@@ -0,0 +1,9 @@
+# 7 minutos y 18 segundos
+
+# En este caso, se aprende a definir y usar una funcion con mas de un argumento, para luego retornar
+# con una operacion aritmetica, en este caso, la suma de dos argumentos 
+def funcion3(x,y):                       #definicion de una funcion con 2 argumentos (x e y)
+    return x+y                           #retorno de una operacion aritmetica (suma) de ambos argumentos
+
+e = funcion3(1,2)                        #llamado  a la funcion definida mediante letra "e", con argumen.
+print (e)                                #impresion de "e", quien llama a la funcion 
