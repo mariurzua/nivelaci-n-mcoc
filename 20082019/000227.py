750dfdbe027c30de84611ed16699a870c286d2a6
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-20 22:46:26 -0400

    Create 000227.py

---
+++ 20082019/000227.py
@@ -0,0 +1,13 @@
+#tiempo video: 1.55
+#Una funcion es una coleccionde estructura o codigos que se puede llamar en el momento de necesitarla
+
+#tiempo video:2.27
+#Aqui se ve como se definio una funcion, y luego se imprime fuera de ella. Con esto, es posible notar
+#que al correr el programa, solo se imprime la parte que esta fuera de la funcion, ya que para que una
+#funcion imprima algo, debe ser "llamada". Lo que aqui se ve es solo la definicion de la funcion.
+#Ademas, se imprime fuera de la funcion una linea
+
+def funcion1():                                #definicion de la funcion
+    print ("holaaaaa")                         #impresion dentro de la funcion de un "holaaaaa"
+    print ("holaaaaaaa2")                      #impresion dentro de la funcion (2da) de un "holaaaa2" 
+print ("estoy fuera de la funcion")            #impresion fuera de la funcion
