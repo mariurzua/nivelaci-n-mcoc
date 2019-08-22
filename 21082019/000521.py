4a9ccb4e1e409e9b6fdbd477bde86c2e193bb539
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-21 20:12:49 -0400

    Create 000521.py

---
+++ 21082019/000521.py
@@ -0,0 +1,10 @@
+# 5 minutos y 21 segundos
+
+# Se aprende en este codigo a que un elemento de la lista puede ser reemplazado por otro. Esto se logra
+# mediante el llamado de la lista y entre los corchetes la posicion del elemento que se desea reemplazar
+# y se iguala a el nuevo elemento. 
+
+a = [3, 10, -1, 1]     #creacion lista "a" con 4 elementos, posiciones 0, 1, 2, 3.
+
+a[0]= 100              #reemplazo elemento en ubicacion 0 de la lista (un 3), por el numero 100.     
+print (a)              #impresion nueva lista con elemento reemplazado.
