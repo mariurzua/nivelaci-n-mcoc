7b7af4764957e1d91042387a47c8ffc6d713ba8b
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-21 20:12:22 -0400

    Create 000405.py

---
+++ 21082019/000405.py
@@ -0,0 +1,10 @@
+#4 minutos y 5 segundos
+
+# Se aprende a imprimir un elemento dentro de la lista. Esto se logra mediante el uso de la posicion, ya
+# que se posicionan segun el orden en que van, partiendo con la posicion 0, 1, 2... y asi.
+
+a = [3, 10, -1, 1]     #creacion lista "a" con 4 elementos, posiciones 0, 1, 2, 3.
+
+print (a[0])           #impresion primer elemento de la lista, correspondiente al ubicado en posicion 0.   
+print (a[1])           #impresion segundo elemento de la lista, correspondiente al ubicado en posicion 1. 
+print (a[3])           #impresion cuarto elemento de la lista, correspondiente al ubicado en posicion 3. 
