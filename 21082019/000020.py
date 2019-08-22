9a5b35c474bf70ea0b8342ae0808271b6694cac3
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-21 20:10:44 -0400

    Create 000020.py

---
+++ 21082019/000020.py
@@ -0,0 +1,17 @@
+# 0 minutos y 20 segundos
+
+# Se aprende en este codigo a formar una lista mediante el uso de corchetes. Luego, mediante el uso
+# de "append" se puede ver como es posible agregar tanto un elemento numerico a la lista, y un elemento
+# correspondiente a una palabra (con el uso de comillas). Las listas coleccionan elementos.
+
+a = [3, 10, -1]    #creacion lista "a", formada por 3 elementos en ubicacion 0, 1 y 2.
+
+print (a)          #impresion lista "a".
+
+a.append(1)        #adicion de un elemento a la lista "a". En este caso, el numero 1.
+
+print (a)          #impresion lista "a". Se observa un elemento mas, o sea, hay posicion 0, 1, 2, 3.
+
+a.append("hola")   #adicion de un elemento a a lista "a". En este caso, palabra "hola" en comillas.
+
+print(a)           #impresion lista "a". Se observa un elemento mas, o sea hay posicion 0, 1, 2, 3, 4
