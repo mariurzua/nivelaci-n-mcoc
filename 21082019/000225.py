29938ec97d2a5ed4f07ae512e1dcda10c0c54ece
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-21 20:11:26 -0400

    Create 000225.py

---
+++ 21082019/000225.py
@@ -0,0 +1,11 @@
+# 2 minutos y 25 segundos 
+
+# En este ejemplo, se puede observar como es  posible añadir una lista como elemento dentro de otra lista.
+# Se agrego una lista de 2 numeros. Esta se agrega automaticamente al final de la lista.
+
+
+a = [3, 10, -1, 1, "hola"]       #creacion lista "a" con 5 elementos, posiciones 0, 1, 2, 3, 4
+
+a.append([1, 2])                 #Se añade una lista dentro de la lista mediante (append)
+
+print(a)                         #impresion de la lista. Se observa al final de la lista. 
