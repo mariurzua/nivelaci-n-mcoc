5323d5e8d4ff54e216319310381cbcfb907d8a04
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-26 21:20:15 -0400

    Create 000348.py

---
+++ 26082019/000348.py
@@ -0,0 +1,11 @@
+# 3 minutos y 48 segundos
+
+# Se aprende aqui, a diferencia de los ejemplos anteriores, que una llave no necesariamente debe ser una palabra
+# entre comillas, sino que tambien puede ser un numero. En este caso, se observa la llave con el numero 10, al 
+# que se le asigna un valor de 300.
+
+d = {}                 #creacion de diccionario "d"
+
+d[10] = 300            #adición de una llave (10) y su valor (300)                     
+
+print(d[10])           #impresion informacion asociada a llave "10", es decir, 300
