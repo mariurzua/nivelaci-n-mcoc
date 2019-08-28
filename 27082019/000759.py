c454f05c9566e11b0434d7956d8eaf7a7cda199e
mariurzua <53714907+mariurzua@users.noreply.github.com>
2019-08-27 21:13:33 -0400

    Create 000759.py

---
+++ 27082019/000759.py
@@ -0,0 +1,18 @@
+# 7 minutos y 59 segundos 
+
+# Considerando la foto anterior, las matrices numpy y funciones descritas abajo (hay mas que solo suma y producto)
+# se aprendio a hacer operaciones entre los elementos de una matriz
+
+import numpy as np  #importacion libreria numpy
+from skimage import io   #importacion io para leer la foto
+import matplotlib.pyplot as plt  #importacion para que salga la imagen
+
+#aplicacion a cada elemento
+foto
+foto_sin = np.sin(foto)   #tomar el seno de cada elemento que compone el numpy
+foto_sin
+
+print(np.sum(foto))    #suma de los elementos de la matriz de foto
+print(np.prod(foto))   #producto de los elementos de la matriz de foto
+
+# Asi, hay mas funciones, varianza, minimo, maximo, arco maximo y minimo etc
